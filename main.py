fe23ab6b31e414e3c2314a63c0204550e441b9c9
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2026-06-23 12:48:49 +0800

    feat: save image to NPY file

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from PIL import Image, ImageOps
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def main() -> None:
-    print("Hello world")
+    image = Image.open("cspace.png")
+    image = ImageOps.grayscale(image)
+
+    # convert to NumPy image
+    np_image = np.array(image)
+    np_image = ~np_image
+    np_image[np_image > 0] = 1
+
+    # save to NPY file
+    np.save("cspace.npy", np_image)
+
+    # open via Matplotlib
+    np_image = np.load("cspace.npy")
+    plt.imshow(np_image, cmap="binary")
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
